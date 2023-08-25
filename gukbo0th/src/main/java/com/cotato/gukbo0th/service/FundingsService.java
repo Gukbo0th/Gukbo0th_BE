a3b805e944ce1911d03625e67407499bade996c0
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2023-08-26 05:37:18 +0900

    feat: 펀딩 상세 페이지 조회 구현
    상세 페이지 조회 구현했습니다.

--- gukbo0th/src/main/java/com/cotato/gukbo0th/service/FundingsService.java
+++ gukbo0th/src/main/java/com/cotato/gukbo0th/service/FundingsService.java
@@ -4,12 +4,14 @@ import com.cotato.gukbo0th.domain.*;
 import com.cotato.gukbo0th.dto.CreditRequestDto;
 import com.cotato.gukbo0th.dto.FundingsDetailResponseDto;
 import com.cotato.gukbo0th.dto.FundingsListResponseDto;
+import com.cotato.gukbo0th.dto.PersonsInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +19,7 @@ public class FundingsService {
 
     private final MembersRepository membersRepository;
     private final FundingsRepository fundingsRepository;
+    private final PersonsRepository personsRepository;
     public List<FundingsListResponseDto> getList() {
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
         List<Fundings> fundings = fundingsRepository.findAll(sort);
@@ -29,9 +32,33 @@ public class FundingsService {
         return responseFundings;
     }
 
+<<<<<<< Updated upstream
 //    public FundingsDetailResponseDto getDetail(Long id) {
 //    }
 //
 //    public FundingsDetailResponseDto credit(CreditRequestDto creditRequestDto) {
 //    }
+=======
+    public FundingsDetailResponseDto getDetail(Long id) throws Exception {
+        try {
+            Fundings fundings = fundingsRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 펀딩 고유 번호"));
+            List<Persons> persons = personsRepository.findByFundings(fundings);
+            List<PersonsInfo> personsInfoList = new ArrayList<>();
+            for(Persons p : persons) {
+                personsInfoList.add(PersonsInfo.builder()
+                        .name(p.getName())
+                        .achieve(p.getAchieve())
+                        .build());
+            }
+            return new FundingsDetailResponseDto(fundings,personsInfoList);
+        }
+        catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public FundingsDetailResponseDto credit(CreditRequestDto creditRequestDto) {
+    }
+>>>>>>> Stashed changes
 }
